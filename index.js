3a0439d73a84d2b601a297c5f9df1de1059344a0
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-03-17 20:17:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+class Polygon {
+  constructor(sidesArray) {
+    this.sidesArray = sidesArray;
+  }
+
+  get getCount() {
+    return this.sidesArray.length;
+  }
+
+  get perimeter() {
+    return this.sidesArray.reduce((a, b) => a + b);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    let maxSide = Math.max(...this.sidesArray);
+    let otherSides = this.perimeter - maxSide;
+
+    return otherSides > maxSide;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.perimeter % 4 == 0
+  }
+
+  get area() {
+    return (this.perimeter / 4) ** 2
+  }
+}
